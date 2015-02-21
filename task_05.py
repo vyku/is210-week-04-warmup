f3a10913f6a6b6362567b5588cb44fd43d716763
vyku visva <vykunthan.visvalingam@spsmail.cuny.edu>
2015-02-21 22:21:44 +0000

    Commiting task 05 need corrections

---
+++ task_05.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A BP project"""
+
+MYINPUT = raw_input('What is your blood pressure? ')
+MYINPUT= int(MYINPUT)
+BP_STATUS = ('Low')
+
+
+if MYINPUT <= 89:
+    print BP_STATUS
+
+    if MYINPUT >=90 or MYINPUT <=119:
+        print BP_STATUS       
+    
+    
+    
+
+OUTPUT = ' Your status is currently:{}'.format(BP_STATUS)
+print OUTPUT
+
+
